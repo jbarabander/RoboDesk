91359f58003ad7cb332888f6d4c703fd4bcf2911
Justin Barabander <jbarabander@gmail.com>
2015-09-28 00:22:15 -0400

    Continued work on bind functions

--- browser/js/directives/add-macro/add-macro.js
+++ browser/js/directives/add-macro/add-macro.js
@@ -7,31 +7,30 @@ app.directive('addMacro', function() {
             steps: "=",
         },
         link: function(scope, element, attributes) {
-            scope.choicesWithOptions = {click: ['right', 'left'], itunes: ['play', 'stop']};
-            scope.actionCategories = ['click', 'enter', 'file', 'itunes', 'keyboard', 'chrome'];
-
-                var count = 0;
-                var step = $('.step').clone();
-                $('#keyboard-shortcut').on('keydown', function (event) {
-                    var numOfPluses;
-                    console.log(event.which);
-                    if ($(this).val() === '') numOfPluses = 0;
-                    else numOfPluses = $(this).val().match(/\+/g) ? $(this).val().match(/\+/g).length : 0;
-                    console.log(numOfPluses);
-                    if ((numOfPluses === 1) || (numOfPluses === 0)) {
-                        if (event.which === 17) $(this).val($(this).val() + 'ctrl+');
-                        else if(event.which === 18) $(this).val($(this).val() + 'alt+');
-                        else if(event.which === 16) $(this).val($(this).val() + 'shift+');
-                        else if(event.which === 8)  $(this).val('');
-                        else return false;
-                    }
-                    else {
-                        var arr = $(this).val().split('+');
-                        arr.splice(0,2);
-                        arr = arr.join('');
-                        if(arr.length >= 1 && event.which !== 8) return false;
-                    }
-                });
+            scope.choicesWithOptions = {click: ['right', 'left'], itunes: ['play', 'stop'], keytap: ['enter', 'tab', 'backspace']};
+            scope.actionCategories = ['click', 'keytap', 'file', 'itunes', 'keyboard', 'browser'];
+            var count = 0;
+            var step = $('.step').clone();
+            $('#keyboard-shortcut').on('keydown', function (event) {
+                var numOfPluses;
+                console.log(event.which);
+                if ($(this).val() === '') numOfPluses = 0;
+                else numOfPluses = $(this).val().match(/\+/g) ? $(this).val().match(/\+/g).length : 0;
+                console.log(numOfPluses);
+                if ((numOfPluses === 1) || (numOfPluses === 0)) {
+                    if (event.which === 17) $(this).val($(this).val() + 'ctrl+');
+                    else if(event.which === 18) $(this).val($(this).val() + 'alt+');
+                    else if(event.which === 16) $(this).val($(this).val() + 'shift+');
+                    else if(event.which === 8)  $(this).val('');
+                    else return false;
+                }
+                else {
+                    var arr = $(this).val().split('+');
+                    arr.splice(0,2);
+                    arr = arr.join('');
+                    if(arr.length >= 1 && event.which !== 8) return false;
+                }
+            });
 
             scope.addStep = function() {
                 scope.steps.push({name: null, action: null});
--- browser/js/factories/macro.js
+++ browser/js/factories/macro.js
@@ -10,35 +10,54 @@ app.factory('MacroFactory', function() {
     //        })
     //}
     function stepEval(step) {
-        console.log('in step eval');
-        if(step.name === 'keyboard') robot.typeString(step.action);
+        console.log(step.action);
+        if(step.name === 'keyboard') {
+            robot.typeString(step.action)
+        }
+        else if(step.name === 'keytap') {
+            if(step.action) {
+                for(var i = 0; i < step.action; i++) {
+                    robot.keyTap(step.option);
+                }
+            }
+            else {
+                robot.keyTap(step.option)
+            }
+        }
+        else if(step.name === 'file') {
+            gui.Shell.openItem(step.action);
+        }
+        else if(step.name === 'browser') {
+            setTimeout(function() {
+                gui.Shell.openExternal(step.action);
+            }, 1000);
+        }
+
     }
     function bindMacro(macroObj) {
         var shortcutSplit = macroObj.shortcut.split('+');
         var keyBinding = shortcutSplit.map(function(element) {
             return element[0].toUpperCase() + element.slice(1);
         }).join('+');
-        console.log(keyBinding);
         var option = {
             key: keyBinding,
             active: function() {
-                console.log('in the active');
-                var steps = macroObj.steps;
-                steps.forEach(function(step) {
-                    console.log('hi');
-                    stepEval(step);
-                })
+                setTimeout(function() {
+                    var steps = macroObj.steps;
+                    steps.forEach(function(step) {
+                        stepEval(step);
+                    })
+                }, 300)
             },
             failed: function(msg) {
                 console.log(msg);
             }
         };
         var shortcut = new gui.Shortcut(option);
-        console.log(shortcut);
         gui.App.registerGlobalHotKey(shortcut);
-        shortcut.on('active', function() {
-            console.log('this is is active');
-        })
+        //shortcut.on('active', function() {
+        //    console.log('this is active');
+        //})
     }
     function loadMacros(macroSetId) {
         return Macro.find({macroCollection: macroSetId});
